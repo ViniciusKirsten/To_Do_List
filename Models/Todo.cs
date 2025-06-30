d06189b62fe804ec7d18c2f061b09e61b823ce4d
Vinicius Kirsten Mendonça <vinicius.kirsten@ska.com.br>
2025-06-30 13:03:13 -0300

    Adicionando o model Todo

---
+++ Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace To_Do_List
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreateAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+        public string User { get; set; }
+    }
+}
