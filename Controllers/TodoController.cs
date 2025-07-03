fc6c1de2d46bb3e6215048937136868316489283
Vinicius Mendonca <vinicius.k.mendonca@outlook.com>
2025-07-02 21:40:56 -0300

    Criando a tabela Todo e criando a migration

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using To_Do_List;
 using ToDoList.Data;
 
-
-namespace ToDoList
+namespace ToDoList.Controllers
 {
     public class TodoController : Controller
     {
